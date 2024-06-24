06aa614f7766512149e7c540f93b1bf16b330e70
Mike Back <hello@mikeback.me>
2024-06-24 12:45:15 +1000

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+import React, { useEffect, useState } from "react";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center p-24">
+      <h1 className="mb-8">Quote Generator</h1>
+    </main>
+  );
+}
