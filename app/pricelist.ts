3405d2f4d8f4832d1c21bb1cc15d7727d4f9c14a
Mike Back <hello@mikeback.me>
2024-06-25 10:23:09 +1000

    feat: add basic pricelist

---
+++ app/pricelist.ts
@@ -0,0 +1,32 @@
+import { PriceListItem } from "./types";
+
+export const pricelist: PriceListItem[] = [
+  {
+    id: "1",
+    offering: "Basic Ad",
+    capability: "Generic Ad that allows the user to change images and text",
+    price: 500,
+    addOn: false,
+  },
+  {
+    id: "2",
+    offering: "Standard Ad",
+    capability: "Standard template offering",
+    price: 1000,
+    addOn: false,
+  },
+  {
+    id: "3",
+    offering: "AI copy writing (Add-on)",
+    capability: `Generate dynamic text based off the userʼs brand`,
+    price: 450,
+    addOn: true,
+  },
+  {
+    id: "4",
+    offering: "AI voice generation (Add-on)",
+    capability: "Enables AI voice over generatio",
+    price: 600,
+    addOn: true,
+  },
+];
--- app/types.ts
+++ app/types.ts
@@ -1,1 +1,12 @@
+export interface PriceListItem {
+  id: string;
+  offering: string;
+  capability: string;
+  price: number;
+  addOn: boolean;
+}
 
+export interface CartItem extends PriceListItem {
+  quantity: number;
+  discountedTotal?: number;
+}
